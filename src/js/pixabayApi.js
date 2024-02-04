8b14f4749488fb005404d879c27e52100a4380fd
Serhii Rybak <serhiirybak@Serhiis-MacBook-Pro.local>
2024-02-04 14:23:08 +0100

    Added some fixes

--- src/js/pixabayApi.js
+++ src/js/pixabayApi.js
@@ -7,6 +7,7 @@ function searchImages({ query, page = 1, per_page = 40 }) {
   const apiQuery = new URLSearchParams({
     q: query,
     page,
+    per_page,
     key: API_KEY,
     image_type: 'photo',
     orientation: 'horizontal',
